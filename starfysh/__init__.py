6f5992d2af0c99451a529a7acac702c46ba403b9
yinuo <yj2589@columbia.edu>
2023-01-18 09:46:54 -0500

    minor fix to show colab loggings

--- starfysh/__init__.py
+++ starfysh/__init__.py
@@ -4,7 +4,7 @@ import logging
 logging.basicConfig(
     level=logging.INFO,
     format='[%(asctime)s] %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
+    datefmt='%Y-%m-%d %H:%M:%S',
     force=True
 )
 
